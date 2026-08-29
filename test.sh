60fdec409421e74784e9f2132fdeedc1e4775179
zuhito <zuhito@users.noreply.github.com>
2026-08-29 12:40:13 +0000

    Run container as root and reuse devcontainer in CI

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-sudo -E "$(which python3)" modbus_test.py >/dev/null 2>&1 &
+nohup python3 modbus_test.py > /tmp/modbus.log 2>&1 &
 until timeout 1 bash -c ": >/dev/tcp/127.0.0.1/502" 2>/dev/null; do sleep 1; done
 nmap -p 502 --script modbus-discover 127.0.0.1
