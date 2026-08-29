7ae371737440dc212a4474c567856e1c5b23031b
zuhito <zuhito@users.noreply.github.com>
2026-08-29 15:14:26 +0000

    Drop postAttachCommand

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-setsid nohup python3 modbus_server.py < /dev/null >> /tmp/modbus.log 2>&1 &
-setsid nohup node-red < /dev/null >> /tmp/nodered.log 2>&1 &
-exit 0
+setsid nohup python3 modbus_server.py < /dev/null > /tmp/modbus.log 2>&1 &
+setsid nohup node-red < /dev/null > /tmp/nodered.log 2>&1 &
