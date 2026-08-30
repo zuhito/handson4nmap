80243b2f661d2577a982425d2efea0c0d8c29173
zuhito <zuhito@users.noreply.github.com>
2026-08-30 16:31:25 +0900

    Update nmap-build.sh

--- scripts/nmap-build.sh
+++ scripts/nmap-build.sh
@@ -2,10 +2,7 @@
 set -e
 cd "$(dirname "$0")/.."
 
-# The nmap packaged for the container image predates multicast-profinet-discovery,
-# so the current release is built from source and installed to /usr/local.
 NMAP_TAG="${NMAP_TAG:-v7.991}"
-
 rm -rf external/nmap
 git clone -q --branch "$NMAP_TAG" --depth 1 https://github.com/nmap/nmap.git external/nmap
 cd external/nmap
