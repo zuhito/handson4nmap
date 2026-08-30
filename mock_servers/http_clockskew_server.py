a0e35c6e74d0be120892c0ea2e00a469129ea5a4
zuhito <zuhito@users.noreply.github.com>
2026-08-30 11:33:55 +0900

    Update http_clockskew_server.py

--- mock_servers/http_clockskew_server.py
+++ mock_servers/http_clockskew_server.py
@@ -1,24 +1,19 @@
 import http.server
 import time
 
-PORT = 8000
-SKEW_SECONDS = 4 * 60 + 51
-
-
 class Handler(http.server.BaseHTTPRequestHandler):
     server_version = "AichiHTTP/1.0"
     sys_version = ""
 
     def date_time_string(self, timestamp=None):
-        return super().date_time_string(time.time() + SKEW_SECONDS)
+        return super().date_time_string(time.time() + 4 * 60 + 51)
 
     def do_GET(self):
-        body = b"clock skew test server\n"
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
         self.send_header("Content-Length", str(len(body)))
         self.end_headers()
-        self.wfile.write(body)
+        self.wfile.write(b"clock skew test server\n")
 
     def do_HEAD(self):
         self.send_response(200)
@@ -30,4 +25,4 @@ class Handler(http.server.BaseHTTPRequestHandler):
         pass
 
 
-http.server.HTTPServer(("0.0.0.0", PORT), Handler).serve_forever()
+http.server.HTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
